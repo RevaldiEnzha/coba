c11e24d33273b0adc473ba74efbd5da6214f10df
bilxdi <biladi.amna@mhs.unsoed.ac.id>
2026-06-11 20:58:18 +0700

    tambah favicon pada tab browser

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistem Laundry</title>
     <link rel="stylesheet" href="{{ asset('css/laundry.css') }}">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 </head>
 <body class="app-body">
     <div class="app-shell">
